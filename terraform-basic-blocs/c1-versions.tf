67da4f5f139afb844b073bdc5051a34c43eebe7f
Selahattinasn <selahattinasn@gmail.com>
2024-03-13 09:05:45 +0100

    updated

--- terraform-basic-blocs/c1-versions.tf
+++ terraform-basic-blocs/c1-versions.tf
@@ -1,4 +1,5 @@
 # Terraform Block
+
 terraform {
   required_version = ">= 1.6" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
   required_providers {
@@ -10,11 +11,11 @@ terraform {
 }  
 # Provider Block
 provider "aws" {
-  region = "us-east-1"
+  region = "eu-central-1"
 }
 
 /*
-Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal  
+Note-1:  AWS Credentials Profile (profile = "default") configured on our local desktop terminal  
 $HOME/.aws/credentials
 */
 
